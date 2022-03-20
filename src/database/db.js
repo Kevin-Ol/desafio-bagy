34bd803de821a5002cb73c29ff613e2e365e2cc5
Kevin Oliveira <kevin.zero@hotmail.com>
2022-03-20 17:43:16 -0300

    Adiciona dados de outras lojas no banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -244,20 +244,220 @@ module.exports = {
       revenues: {
         2020: {
           june: {
-            total: 0,
+            total: 51236,
             daily: [
               {
                 day: 1,
-                value: 0,
+                value: 15268,
+              },
+              {
+                day: 2,
+                value: 14752,
+              },
+              {
+                day: 3,
+                value: 19352,
+              },
+              {
+                day: 4,
+                value: 22485,
+              },
+              {
+                day: 5,
+                value: 19622,
+              },
+              {
+                day: 6,
+                value: 27456,
+              },
+              {
+                day: 7,
+                value: 30200,
+              },
+              {
+                day: 8,
+                value: 32451,
+              },
+              {
+                day: 9,
+                value: 38744,
+              },
+              {
+                day: 10,
+                value: 41265,
+              },
+              {
+                day: 11,
+                value: 36185,
+              },
+              {
+                day: 12,
+                value: 32014,
+              },
+              {
+                day: 13,
+                value: 29568,
+              },
+              {
+                day: 14,
+                value: 34522,
+              },
+              {
+                day: 15,
+                value: 35416,
+              },
+              {
+                day: 16,
+                value: 38762,
+              },
+              {
+                day: 17,
+                value: 33962,
+              },
+              {
+                day: 18,
+                value: 32157,
+              },
+              {
+                day: 19,
+                value: 31232,
+              },
+              {
+                day: 20,
+                value: 28845,
+              },
+              {
+                day: 21,
+                value: 26521,
+              },
+              {
+                day: 22,
+                value: 24103,
+              },
+              {
+                day: 22,
+                value: 20157,
+              },
+              {
+                day: 23,
+                value: 25100,
+              },
+              {
+                day: 24,
+                value: 28451,
+              },
+              {
+                day: 25,
+                value: 30268,
               },
             ],
           },
           july: {
-            total: 0,
+            total: 42568,
             daily: [
               {
                 day: 1,
-                value: 0,
+                value: 20514,
+              },
+              {
+                day: 2,
+                value: 24758,
+              },
+              {
+                day: 3,
+                value: 26532,
+              },
+              {
+                day: 4,
+                value: 30215,
+              },
+              {
+                day: 5,
+                value: 29665,
+              },
+              {
+                day: 6,
+                value: 32415,
+              },
+              {
+                day: 7,
+                value: 31526,
+              },
+              {
+                day: 8,
+                value: 27415,
+              },
+              {
+                day: 9,
+                value: 22410,
+              },
+              {
+                day: 10,
+                value: 20153,
+              },
+              {
+                day: 11,
+                value: 18378,
+              },
+              {
+                day: 12,
+                value: 15269,
+              },
+              {
+                day: 13,
+                value: 14225,
+              },
+              {
+                day: 14,
+                value: 17410,
+              },
+              {
+                day: 15,
+                value: 22153,
+              },
+              {
+                day: 16,
+                value: 28741,
+              },
+              {
+                day: 17,
+                value: 33695,
+              },
+              {
+                day: 18,
+                value: 36841,
+              },
+              {
+                day: 19,
+                value: 31025,
+              },
+              {
+                day: 20,
+                value: 22415,
+              },
+              {
+                day: 21,
+                value: 24851,
+              },
+              {
+                day: 22,
+                value: 30256,
+              },
+              {
+                day: 22,
+                value: 36841,
+              },
+              {
+                day: 23,
+                value: 40259,
+              },
+              {
+                day: 24,
+                value: 45102,
+              },
+              {
+                day: 25,
+                value: 51420,
               },
             ],
           },
@@ -271,20 +471,220 @@ module.exports = {
       revenues: {
         2020: {
           june: {
-            total: 0,
+            total: 48741,
             daily: [
               {
                 day: 1,
-                value: 0,
+                value: 25741,
+              },
+              {
+                day: 2,
+                value: 26851,
+              },
+              {
+                day: 3,
+                value: 30263,
+              },
+              {
+                day: 4,
+                value: 32015,
+              },
+              {
+                day: 5,
+                value: 35784,
+              },
+              {
+                day: 6,
+                value: 29510,
+              },
+              {
+                day: 7,
+                value: 26410,
+              },
+              {
+                day: 8,
+                value: 18541,
+              },
+              {
+                day: 9,
+                value: 13201,
+              },
+              {
+                day: 10,
+                value: 11984,
+              },
+              {
+                day: 11,
+                value: 15410,
+              },
+              {
+                day: 12,
+                value: 17445,
+              },
+              {
+                day: 13,
+                value: 29568,
+              },
+              {
+                day: 14,
+                value: 22153,
+              },
+              {
+                day: 15,
+                value: 25186,
+              },
+              {
+                day: 16,
+                value: 33625,
+              },
+              {
+                day: 17,
+                value: 39514,
+              },
+              {
+                day: 18,
+                value: 31206,
+              },
+              {
+                day: 19,
+                value: 24851,
+              },
+              {
+                day: 20,
+                value: 21475,
+              },
+              {
+                day: 21,
+                value: 27410,
+              },
+              {
+                day: 22,
+                value: 31520,
+              },
+              {
+                day: 22,
+                value: 42610,
+              },
+              {
+                day: 23,
+                value: 37452,
+              },
+              {
+                day: 24,
+                value: 34268,
+              },
+              {
+                day: 25,
+                value: 27456,
               },
             ],
           },
           july: {
-            total: 0,
+            total: 37852,
             daily: [
               {
                 day: 1,
-                value: 0,
+                value: 12468,
+              },
+              {
+                day: 2,
+                value: 10932,
+              },
+              {
+                day: 3,
+                value: 17217,
+              },
+              {
+                day: 4,
+                value: 20420,
+              },
+              {
+                day: 5,
+                value: 30903,
+              },
+              {
+                day: 6,
+                value: 33740,
+              },
+              {
+                day: 7,
+                value: 32798,
+              },
+              {
+                day: 8,
+                value: 38046,
+              },
+              {
+                day: 9,
+                value: 24054,
+              },
+              {
+                day: 10,
+                value: 18653,
+              },
+              {
+                day: 11,
+                value: 16475,
+              },
+              {
+                day: 12,
+                value: 15360,
+              },
+              {
+                day: 13,
+                value: 29827,
+              },
+              {
+                day: 14,
+                value: 28748,
+              },
+              {
+                day: 15,
+                value: 20591,
+              },
+              {
+                day: 16,
+                value: 22425,
+              },
+              {
+                day: 17,
+                value: 38280,
+              },
+              {
+                day: 18,
+                value: 40085,
+              },
+              {
+                day: 19,
+                value: 48512,
+              },
+              {
+                day: 20,
+                value: 52684,
+              },
+              {
+                day: 21,
+                value: 43302,
+              },
+              {
+                day: 22,
+                value: 38362,
+              },
+              {
+                day: 22,
+                value: 37124,
+              },
+              {
+                day: 23,
+                value: 27495,
+              },
+              {
+                day: 24,
+                value: 18636,
+              },
+              {
+                day: 25,
+                value: 31176,
               },
             ],
           },
@@ -298,20 +698,220 @@ module.exports = {
       revenues: {
         2020: {
           june: {
-            total: 0,
+            total: 52415,
             daily: [
               {
                 day: 1,
-                value: 0,
+                value: 26045,
+              },
+              {
+                day: 2,
+                value: 12277,
+              },
+              {
+                day: 3,
+                value: 26449,
+              },
+              {
+                day: 4,
+                value: 35879,
+              },
+              {
+                day: 5,
+                value: 25189,
+              },
+              {
+                day: 6,
+                value: 24339,
+              },
+              {
+                day: 7,
+                value: 37128,
+              },
+              {
+                day: 8,
+                value: 31839,
+              },
+              {
+                day: 9,
+                value: 34691,
+              },
+              {
+                day: 10,
+                value: 29443,
+              },
+              {
+                day: 11,
+                value: 35198,
+              },
+              {
+                day: 12,
+                value: 27858,
+              },
+              {
+                day: 13,
+                value: 22128,
+              },
+              {
+                day: 14,
+                value: 30784,
+              },
+              {
+                day: 15,
+                value: 25186,
+              },
+              {
+                day: 16,
+                value: 38808,
+              },
+              {
+                day: 17,
+                value: 34951,
+              },
+              {
+                day: 18,
+                value: 44579,
+              },
+              {
+                day: 19,
+                value: 44141,
+              },
+              {
+                day: 20,
+                value: 49972,
+              },
+              {
+                day: 21,
+                value: 51079,
+              },
+              {
+                day: 22,
+                value: 51485,
+              },
+              {
+                day: 22,
+                value: 36054,
+              },
+              {
+                day: 23,
+                value: 41142,
+              },
+              {
+                day: 24,
+                value: 38415,
+              },
+              {
+                day: 25,
+                value: 35269,
               },
             ],
           },
           july: {
-            total: 0,
+            total: 84152,
             daily: [
               {
                 day: 1,
-                value: 0,
+                value: 31968,
+              },
+              {
+                day: 2,
+                value: 46343,
+              },
+              {
+                day: 3,
+                value: 37395,
+              },
+              {
+                day: 4,
+                value: 34283,
+              },
+              {
+                day: 5,
+                value: 17459,
+              },
+              {
+                day: 6,
+                value: 18926,
+              },
+              {
+                day: 7,
+                value: 20022,
+              },
+              {
+                day: 8,
+                value: 34098,
+              },
+              {
+                day: 9,
+                value: 45191,
+              },
+              {
+                day: 10,
+                value: 46282,
+              },
+              {
+                day: 11,
+                value: 51159,
+              },
+              {
+                day: 12,
+                value: 50574,
+              },
+              {
+                day: 13,
+                value: 328182,
+              },
+              {
+                day: 14,
+                value: 42134,
+              },
+              {
+                day: 15,
+                value: 48953,
+              },
+              {
+                day: 16,
+                value: 47479,
+              },
+              {
+                day: 17,
+                value: 43910,
+              },
+              {
+                day: 18,
+                value: 36781,
+              },
+              {
+                day: 19,
+                value: 44168,
+              },
+              {
+                day: 20,
+                value: 45442,
+              },
+              {
+                day: 21,
+                value: 42149,
+              },
+              {
+                day: 22,
+                value: 35524,
+              },
+              {
+                day: 22,
+                value: 35147,
+              },
+              {
+                day: 23,
+                value: 24878,
+              },
+              {
+                day: 24,
+                value: 27111,
+              },
+              {
+                day: 25,
+                value: 29452,
               },
             ],
           },
